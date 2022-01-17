2a2be8295951c9ab8f3550a28dd62a6c8243d47f
Allison Valdez <ajsnipes@usfca.edu>
2022-01-17 12:44:11 -0800

    almost done with code

--- Mod1/__main__.py
+++ Mod1/__main__.py
@@ -1,10 +1,12 @@
 """
 Allison Valdez
-Time complexity:
-Space complexity:
+Time complexity: O(n) where n is the number of competitions.
+Space complexity: O(k) where k is the number of k teams the 30 characters
+does not matter in the analysis.
 """
 
-home_team_wins = 1
+home_team_won = 1
+
 def tournament_winner(competitions, results):
     """
     The competitions and results are two different arrays that contain the
@@ -20,8 +22,24 @@ def tournament_winner(competitions, results):
 
     :param competitions: array of the competition round for each team
     :param results: array of the outcome of the competitions
-    :return: the best team that won the competition
+    :return: the best team that won the full competition
     """
+    current_best_team = ""
+    scores = {current_best_team: 0}
+
+    for index, current_round in enumerate(competitions):
+        current_result = results[index]
+        home_team, away_team = current_round
+
+        winning_team = home_team if current_result == home_team_won else away_team
+
+        update_scores(winning_team, 3, scores)
+
+        if scores[winning_team] > scores[current_best_team]
+            current_best_team = winning_team
+
+    return current_best_team
+
 
 def update_scores(team, points, scores):
     """
