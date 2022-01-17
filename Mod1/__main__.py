431553f5509274ce867be830fd82c9680a290c94
Allison Valdez <ajsnipes@usfca.edu>
2022-01-16 19:33:33 -0800

    working on setting up the problem

--- Mod1/__main__.py
+++ Mod1/__main__.py
@@ -7,18 +7,27 @@ Space complexity:
 home_team_wins = 1
 def tournament_winner(competitions, results):
     """
+    The competitions and results are two different arrays that contain the
+    competition details.
 
-    :param competitions:
-    :param results:
-    :return:
-    """
-
-    """
     about .enumerate():
-    when dealing with iterators, we also get a need to keep a count of 
-    iterations. Python eases the programmers’ task by providing a built-in 
-    function enumerate() for this task. Enumerate() method adds a counter to 
-    an iterable and returns it in a form of enumerating object. This 
+    when dealing with iterators, we also get a need to keep a count of
+    iterations. Python eases the programmers’ task by providing a built-in
+    function enumerate() for this task. Enumerate() method adds a counter to
+    an iterable and returns it in a form of enumerating object. This
     enumerated object can then be used directly for loops or converted into a
     list of tuples using the list() method.
+
+    :param competitions: array of the competition round for each team
+    :param results: array of the outcome of the competitions
+    :return: the best team that won the competition
+    """
+
+def update_scores(team, points, scores):
+    """
+
+    :param team:
+    :param points:
+    :param scores:
+    :return:
     """
