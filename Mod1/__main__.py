021d39fdf0593f67d1aa0589183b3bfd71512dbb
Allison Valdez <ajsnipes@usfca.edu>
2022-01-17 14:15:28 -0800

    done adding print statement

--- Mod1/__main__.py
+++ Mod1/__main__.py
@@ -7,6 +7,7 @@ does not matter in the analysis.
 
 home_team_won = 1
 
+
 def tournament_winner(competitions, results):
     """
     The competitions and results are two different arrays that contain the
@@ -25,17 +26,19 @@ def tournament_winner(competitions, results):
     :return: the best team that won the full competition
     """
     current_best_team = ""
+
     scores = {current_best_team: 0}
 
     for index, current_round in enumerate(competitions):
         current_result = results[index]
+
         home_team, away_team = current_round
 
         winning_team = home_team if current_result == home_team_won else away_team
 
         update_scores(winning_team, 3, scores)
 
-        if scores[winning_team] > scores[current_best_team]
+        if scores[winning_team] > scores[current_best_team]:
             current_best_team = winning_team
 
     return current_best_team
@@ -43,9 +46,16 @@ def tournament_winner(competitions, results):
 
 def update_scores(team, points, scores):
     """
+    This function is in charge of checking whether we have a team or not (
+    that has recently scored). If the team does not exists then we initialize
+    it with a 0 score, and add their 3 points of scoring the points.
 
-    :param team:
-    :param points:
-    :param scores:
-    :return:
+    :param team: new team that scored (not in our list of teams that competed
+    :param points: the 3 points scored by the team
+    :param scores: the new teams most up to date score
+    :return: the new team with the correct competition outcome for the round
     """
+    if team not in scores:
+        scores[team] = 0
+
+    scores[team] += points
